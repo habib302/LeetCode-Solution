88714ec22e28174e39fa9cfebfb3b0494e335b3d
MD Pear E Habib <habib.austcse31@gmail.com>
2021-03-16 16:36:40 +0600

    fixed print LCS problem

--- DP/LCS/LCSdpPrint.cpp
+++ DP/LCS/LCSdpPrint.cpp
@@ -27,23 +27,30 @@ int main()
 
 void LCS(string x,string y,int n,int m)
 {
-    bool dp[n+1][m+1];
-    string r;
+    int dp[n+1][m+1];
+    
     for(int i=0;i<(n+1);i++){
         for(int j=0;j<(m+1);j++){
-            if(i==0) dp[i][j]=false;
-            else if(j==0) dp[i][j]=false;
-            else if(x[i-1]==y[j-1]) {dp[i][j]=true; r.push_back(x[i-1]);}
-            else dp[i][j] = dp[i][j-1] || dp[i-1][j];
+            if(i==0) dp[i][j]=0;
+            else if(j==0) dp[i][j]=0;
+            else if(x[i-1]==y[j-1]) dp[i][j] = 1+dp[i-1][j-1];
+            else dp[i][j]= max(dp[i][j-1],dp[i-1][j]);
         }
     }
 
-    for(int i=0;i<(n+1);i++){
-        for(int j=0;j<(m+1);j++){
-            if(dp[i][j]==true) cout<<dp[i][j];
+    int i=n,j=m;
+    string s;
+    while(i>0 && j>0){
+        if(x[i-1]==y[j-1]){
+            s.push_back(x[i-1]);
+            i--;j--;
+        }else if(dp[i][j-1]>dp[i-1][j]){
+            j--;
+        }else{
+            i--;
         }
-        cout<<'\n';
     }
-    
-    cout<<r<<'\n';
+
+    reverse(s.begin(),s.end());
+    cout<<s<<'\n';
 }
