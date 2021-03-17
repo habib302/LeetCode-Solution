472dbc4cb84f13f81560ea31fe70d0c3116768b5
Md Pear E Habib <habib.austcse31@gmail.com>
2021-03-17 09:37:05 +0600

    longest common substring print

--- DP/LCS/LCSubstringPrint.cpp
+++ DP/LCS/LCSubstringPrint.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-int LCSubstring(string x,string y,int n,int m);
+void LCSubstring(string x,string y,int n,int m);
 
 int main()
  {
@@ -19,35 +19,41 @@ int main()
 
         int n=x.size(),m=y.size();
         
-        int result=LCSubstring(x,y,n,m);
-        cout<<result<<'\n';
-        
+        LCSubstring(x,y,n,m);
     }
 	return 0;
 }
 
-int LCSubstring(string x,string y,int n,int m)
+void LCSubstring(string x,string y,int n,int m)
 {
     int dp[n+1][m+1];
     int len=0; //max length=0 hote pare,negative na
+    int r=0,c=0;
     for(int i=0;i<(n+1);i++){
         for(int j=0;j<(m+1);j++){
             if(i==0) dp[i][j]=0;
             else if(j==0) dp[i][j]=0;
             else if(x[i-1]==y[j-1]) {
                 dp[i][j] = 1+dp[i-1][j-1]; 
-                len=max(len,dp[i][j]);
+                //len=max(len,dp[i][j]);
+                if(dp[i][j]>len){
+                    len=dp[i][j];
+                    r=i;
+                    c=j;
+                }
             }
             else dp[i][j]= 0;
         }
     }
-    // for(int i=0;i<(n+1);i++){
-    //     for(int j=0;j<(m+1);j++){
-    //         cout<<dp[i][j]<<" ";
-    //     }
-    //     cout<<'\n';
-    // }
 
-    
-    return len;
+    string s="";
+
+    while(dp[r][c] != 0){
+        s.push_back(x[r-1]);
+        r--;c--;
+    }
+
+    reverse(s.begin(),s.end());
+    cout<<s<<'\n';
+    return;
 }
