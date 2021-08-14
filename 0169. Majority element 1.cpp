d80e19684b4a6a65312038460f613bb80cb1c54f
Md Pear E Habib <habib.austcse31@gmail.com>
2021-08-14 08:01:02 +0600

    169. Majority element 1

---
+++ 0169. Majority element 1.cpp
@@ -0,0 +1,57 @@
+//using map
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        int result=0;
+        for(auto i:m){
+            if(i.second>(nums.size())/2)
+                result=i.first;
+        }
+        
+        return result;
+    }
+};
+
+
+------------------------------------------------
+//using moore-voting algorithm
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int majority=0,count=0;
+        
+        for(int i=0;i<nums.size();i++){
+            
+            if(count==0){
+                majority=nums[i];
+                count++;
+            }
+            else if(majority==nums[i]){ 
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        
+        return majority;
+    }
+};
+
+
+------------------------------------------------
+//using sort
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int m=nums.size()/2;
+        return nums[m];
+    }
+};
